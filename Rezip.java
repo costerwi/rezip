f02187a136dcc76fc82a86c3486f144078dde523
hoijui <hoijui.quaero@gmail.com>
2019-03-01 20:57:41 +0100

    apply static code analysis suggestions & convert to Java 8
    Java 7 is not even available anymore for download from Oracle.

--- Rezip.java
+++ Rezip.java
@@ -14,63 +14,63 @@
  * limitations under the License.
  */
 
-import java.io.*;
-import java.util.zip.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.CRC32;
+import java.util.zip.CheckedOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class Rezip {
     /**
      * Read zip format file from stdin and write new zip to stdout.
      *
-     * With the --store command line argument the output will be an 
+     * With the --store command line argument the output will be an
      * uncompressed zip.
      * Uncompressed files are stored more efficiently in Git.
-     * {@link https://github.com/costerwi/rezip}
+     * {@see https://github.com/costerwi/rezip}
      */
-    public static void main(String argv[]) throws IOException {
+    public static void main(final String[] argv) throws IOException {
         int compression = ZipEntry.DEFLATED;
-        for (String arg : argv) {
+        for (final String arg : argv) {
             if (arg.equals("--store")) {
                 compression = ZipEntry.STORED;
             } else {
-                System.err.println("Usage: Rezip {--store} <in.zip >out.zip");
+                System.err.printf("Usage: %s {--store} <in.zip >out.zip\n", Rezip.class.getSimpleName());
                 System.exit(1);
             }
         }
 
-        ZipInputStream source_zip = new ZipInputStream(System.in);
-        ZipOutputStream dest_zip = new ZipOutputStream(System.out);
-
-        byte[] buffer = new byte[8192];
+        final byte[] buffer = new byte[8192];
         ZipEntry entry;
-        ByteArrayOutputStream uncomp_bs = new ByteArrayOutputStream();
-        CRC32 cksum = new CRC32();
-        CheckedOutputStream uncomp_os = new CheckedOutputStream(uncomp_bs, cksum);
-        try {
+        final ByteArrayOutputStream uncompressed_bs = new ByteArrayOutputStream();
+        final CRC32 checksum = new CRC32();
+        final CheckedOutputStream uncompressed_os = new CheckedOutputStream(uncompressed_bs, checksum);
+        try (final ZipInputStream source_zip = new ZipInputStream(System.in); final ZipOutputStream dest_zip = new ZipOutputStream(System.out)) {
             while ((entry = source_zip.getNextEntry()) != null) {
-                uncomp_bs.reset();
-                cksum.reset();
+                uncompressed_bs.reset();
+                checksum.reset();
 
-                // Copy file from source_zip into uncompressed, checksummed output stream
-                int len = 0;
+                // Copy file from source_zip into uncompressed, check-summed output stream
+                int len;
                 while ((len = source_zip.read(buffer)) > 0) {
-                    uncomp_os.write(buffer, 0, len);
+                    uncompressed_os.write(buffer, 0, len);
                 }
                 source_zip.closeEntry();
 
                 // Modify zip entry for destination zip
-                entry.setSize(uncomp_bs.size());
-                entry.setCrc(cksum.getValue());
+                entry.setSize(uncompressed_bs.size());
+                entry.setCrc(checksum.getValue());
                 entry.setMethod(compression);
                 entry.setCompressedSize(-1); // Unknown compressed size
 
                 // Copy uncompressed file into destination zip
                 dest_zip.putNextEntry(entry);
-                uncomp_bs.writeTo(dest_zip);
+                uncompressed_bs.writeTo(dest_zip);
                 dest_zip.closeEntry();
             }
-        } finally {
-            source_zip.close();
-            dest_zip.close();
         }
     }
 }
+
